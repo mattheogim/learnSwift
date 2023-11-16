77a24f68d3d25c620fd749185ce66eb2e5975fb6
Mattheo <mattheogim@gmail.com>
2023-11-15 20:19:23 -0800

    programmingBasic

---
+++ aHelloWorld/1-2-HelloSwift.playground/Contents.swift
@@ -0,0 +1,111 @@
+import UIKit
+
+
+/*:
+ # Programming Basic
+ */
+/*:
+ ## 4) 주석(Comment)
+* 컴퓨터가 코드를 읽지 않아도 된다고(무시해도 된다고) 알려주는 부분
+---
+*/
+//: >   //  : 한 줄 주석,  /* */ : 여러줄 주석
+
+print("Hello, Swift!")
+//print("Hello, Swift!")
+
+// 여기는 주석을 표시하고 있습니다.
+
+/*
+ print("Hello, Swift!")
+ print("Hello, world!")
+
+ */
+
+
+/* 저도 이제 개발자가 되고 싶어요
+ 개발자가 되는 방법을 알려주세요. */
+
+
+/*
+ This is the start of the first multiline comment.
+ /* This is the second, nested multiline comment. */
+ This is the end of the first multiline comment.
+ */
+
+
+
+
+/*:
+ ## 5) 세미콜론(;)규칙 (Semicolon)
+* 스위프트(Swift)에서 각 라인의 마지막에 붙는 세미콜론(;)은 선택사항
+---
+*/
+//  (대부분의 프로그래밍 언어는 세미콜론 필수)
+//  하나의 라인에 여러가지 명령문을 사용하고 싶을 경우에는 필수
+
+
+var name = "스위프트"
+print("Hello, world")
+
+
+
+//print("안녕"); print("드디어"); print("스위프트 공부를 시작한다.");
+//print("");
+//print("");
+//print("");
+
+
+
+
+
+/*:
+## 6) 줄 맞추기!
+* 일반적으로 중괄호 안에서 들여쓰기(Indentation)를 통해, 코드의 가독성을 높임
+---
+*/
+// 관습적으로 많이 쓰는 부분이므로, 앞으로 하나 하나 보면서 알아갈 예정
+//: > 들여쓰기는 코드의 가독성을 높이기 위한 프로그래머들간의 약속
+
+// 4칸 들여쓰기는 기본 설정
+
+if true {
+    print("안녕하세요")
+}
+
+
+
+
+
+
+
+
+/*:
+ # Now We Start Programming!
+ */
+/*:
+ ## Hello, Swift를 출력해보자!
+* 모든 첫 프로그래밍의 시작은 출력부터 시작
+---
+*/
+
+// 출력을 위한 함수
+
+print("Hello, World")
+
+print("안녕하세요")
+
+print(5)
+
+//print(3 + 4)
+
+//print(3.14)
+
+
+
+
+
+
+
+
+
